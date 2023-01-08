d09bd7fa77958f601924eb5a7d7bec8a1a56a109
ju-voro <voronetskajaj@hotmail.com>
2023-01-08 19:48:50 +0100

    semicolon

--- objects/albumChallenge.js
+++ objects/albumChallenge.js
@@ -22,13 +22,13 @@ const recordCollection = {
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
   if (prop !== 'tracks' && value !== "") {
-    records[id][prop] = value
+    records[id][prop] = value;
   } else if (prop === 'tracks' && !records[id].hasOwnProperty('tracks')) {
-    records[id][prop] = [value]
+    records[id][prop] = [value];
   } else if (prop === 'tracks' && value !== "") {
-    records[id][prop].push(value)
+    records[id][prop].push(value);
   } else if (value === "") {
-    delete records[id][prop]
+    delete records[id][prop];
   }
   return records;
 
